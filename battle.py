99e7e8b171d5a55352bafc4df7ca9966021003ea
sm0ze <smoze12345@gmail.com>
2021-11-24 18:09:42 +1100

    base stats update, added rank 10 bonuses

--- battle.py
+++ battle.py
@@ -38,7 +38,7 @@ class player:
         self.ev = EV + self.calcEV()
         self.act = ACT + self.calcACT()
 
-    def iniCalc(self):
+    def iniCalc(self) -> None:
         statDict = {}
         for enhan in self.sG[0][2]:
             debug("enhan:", enhan)
@@ -80,10 +80,16 @@ class player:
 
     def calcHP(self) -> int:
         # Health - Pai 5 (50) (+10/turn)
-        return 5 * self._pai + self._reg
+        ret = 5 * self._pai + self._reg
+        if self._pai == 10:
+            ret += self._pai
+        return ret
 
     def calcREC(self) -> int:
-        return 2 * self._reg
+        ret = 2 * self._reg
+        if self._reg == 10:
+            ret += int(self._reg * 0.5)
+        return ret
 
     def calcPA(self) -> int:
         # Physical Attack - Str/4th 4 Spd 2 (60)
@@ -91,11 +97,19 @@ class player:
             co = self._4th
         else:
             co = self._str
-        return self._spe * 1 + co * 4
+        ret = self._spe * 1 + co * 4
+        if self._spe == 10:
+            ret += int(self._spe * 0.1)
+        if co == 10:
+            ret += 5
+        return ret
 
     def calcPD(self) -> int:
         # Physical Defense - End 4 Spd 2 (60)
-        return self._end * 4 + self._spe * 1
+        ret = self._end * 4 + self._spe * 1
+        if self._end == 10:
+            ret += int(self._end * 0.5)
+        return ret
 
     def calcMA(self) -> int:
         # Mental Attack - Mem/Int 4 Cel 2 (60)
@@ -103,11 +117,19 @@ class player:
             co = self._int
         else:
             co = self._mem
-        return self._cel * 1 + co * 4
+        ret = self._cel * 1 + co * 4
+        if self._cel == 10:
+            ret += int(self._cel * 0.1)
+        if co == 10:
+            ret += 5
+        return ret
 
     def calcMD(self) -> int:
         # Mental Defense - Cla 4 Cel 2 (60)
-        return self._cla * 4 + self._cel * 1
+        ret = self._cla * 4 + self._cel * 1
+        if self._cla == 10:
+            ret += int(self._cla * 0.5)
+        return ret
 
     def calcAC(self) -> int:
         # Accuracy - Vis/Omn 4 Aur/Olf 3 Gus/Tac 1 Pro 2 (100)
@@ -115,24 +137,55 @@ class player:
             co = self._omn
         else:
             co = self._vis
-        return (
+        ret = (
             4 * co
             + 3 * (self._aur + self._olf)
             + 2 * self._pro
             + 1 * (self._gus + self._tac)
         )
+        if co == 10:
+            ret += 8
+        if self._aur == 10:
+            ret += int(self._aur * 0.5)
+        if self._olf == 10:
+            ret += int(self._olf * 0.5)
+        if self._gus == 10:
+            ret += int(self._gus * 0.3)
+        if self._tac == 10:
+            ret += int(self._tac * 0.3)
+        if self._pro == 10:
+            ret += int(self._pro * 0.4)
+        return ret
 
     def calcEV(self) -> int:
         # Evasion - Inv 4 Aur/Olf 1 Gus/Tac 3 Pro 2 (100)
-        return (
+        ret = (
             4 * self._inv
             + 3 * (self._tac + self._gus)
             + 2 * self._pro
             + 1 * (self._aur + self._olf)
         )
+        if self._inv == 10:
+            ret += int(self._inv * 0.8)
+        if self._aur == 10:
+            ret += int(self._aur * 0.3)
+        if self._olf == 10:
+            ret += int(self._olf * 0.3)
+        if self._gus == 10:
+            ret += int(self._gus * 0.5)
+        if self._tac == 10:
+            ret += int(self._tac * 0.5)
+        if self._pro == 10:
+            ret += int(self._pro * 0.4)
+        return ret
 
     def calcACT(self) -> int:
-        return self._spe + self._cel
+        ret = self._spe + self._cel
+        if self._spe == 10:
+            ret += int(self._spe * 0.2)
+        if self._cel == 10:
+            ret += int(self._cel * 0.2)
+        return ret
 
     def bStat(self):
         return (
